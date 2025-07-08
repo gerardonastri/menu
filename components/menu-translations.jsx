2e33293b46f5cf4b8a5c038111ca153671594014
gerardonastri <gerardonastri06@gmail.com>
2025-07-08 18:38:43 +0200

    traslations added

--- components/menu-translations.jsx
+++ components/menu-translations.jsx
@@ -10,21 +10,49 @@ export const menuItemTranslations = {
   "Lemon soda": "Lemon Soda",
   "Succhi di frutta": "Fruit Juices",
   "Acqua tonica": "Tonic Water",
+  Sprite: "Sprite",
+  Aranciata: "Orange Soda",
+  Chinotto: "Chinotto",
 
   // Birre
   "Birra Peroni 33cl": "Peroni Beer 33cl",
   "Birra Nastro Azzurro 33cl": "Nastro Azzurro Beer 33cl",
   "Birra Nastro Azzurro Capri": "Nastro Azzurro Capri Beer",
+  "Birra Corona": "Corona Beer",
+  "Birra Heineken": "Heineken Beer",
 
   // Food - Pinse
   Italia: "Italia",
   Marinaia: "Marinaia",
+  Marinaio: "Marinaio",
   Vegetariana: "Vegetarian",
   Golosa: "Golosa",
   Siciliana: "Sicilian",
   Olivia: "Olivia",
   Zucchina: "Zucchini",
+  Zucchini: "Zucchini",
   Veg: "Veg",
+  "* Supplemento di 1€ per ogni aggiunta ":
+    "* Supplement of €1 for each addition",
+
+  // Food - Piadine
+  "Prosciutto e Formaggio": "Prosciutto and Cheese",
+  "Salame e Formaggio": "Salami and Cheese",
+  "Mortadella e Formaggio": "Mortadella and Cheese",
+
+  // Food - Panini
+  "Crudo e Sottiletta": "Prosciutto and Cheese",
+  "Cotto e Mozzarella": "Ham and Mozzarella",
+
+  // Food - Bruschettone
+  "Stracciata dei Monti Lattari e Prosciutto Cotto":
+    "Monti Lattari Stracciatella and Cooked Ham",
+  "Stracciata dei Monti Lattari e Prosciutto Crudo":
+    "Monti Lattari Stracciatella and Prosciutto",
+  "Stracciata dei Monti Lattari e Salame":
+    "Monti Lattari Stracciatella and Salami",
+  "Stracciata dei Monti Lattari e Mortadella":
+    "Monti Lattari Stracciatella and Mortadella",
 
   // Food - Toast
   "Cotto e Sottiletta": "Ham and Cheese",
@@ -34,53 +62,94 @@ export const menuItemTranslations = {
 
   // Food - Fritti
   Misto: "Mixed Fried",
+  Patatine: "French Fries",
+  Alghe: "Seaweed",
+  "Anelli di Cipolla": "Onion Rings",
+  "Mozzarella in Carrozza": "Fried Mozzarella",
 
   // Caffetteria
   Espresso: "Espresso",
   "Espresso macchiato": "Macchiato Espresso",
+  "Macchiato Espresso": "Macchiato Espresso",
   Americano: "Americano",
   Decaffeinato: "Decaffeinated",
   Cappuccino: "Cappuccino",
   "Latte macchiato": "Latte Macchiato",
+  "Latte Macchiato": "Latte Macchiato",
   "Latte Freddo": "Cold Milk",
   Thè: "Tea",
+  Tea: "Tea",
   Tisane: "Herbal Teas",
+  "Herbal Teas": "Herbal Teas",
   Camomilla: "Chamomile",
+  Chamomile: "Chamomile",
+  "Caffè Corretto": "Espresso with Liqueur",
+  "Caffè Shakerato": "Shaken Iced Coffee",
 
   // Cocktail
   "Aperol Spritz": "Aperol Spritz",
   "Campari Spritz": "Campari Spritz",
   "Hugò Spritz": "Hugo Spritz",
+  "Hugo Spritz": "Hugo Spritz",
   "Limoncello Spritz": "Limoncello Spritz",
   Negroni: "Negroni",
   "Negroni Sbagliato": "Negroni Sbagliato",
   Americano: "Americano",
   "Gin & Tonic": "Gin & Tonic",
+  "Gin Tonic": "Gin & Tonic",
   "Hendrick's & Tonic": "Hendrick's & Tonic",
   "Capri & Tonic": "Capri & Tonic",
   "Gin & Lemon": "Gin & Lemon",
+  "Gin Lemon": "Gin & Lemon",
   "Vodka & Tonic": "Vodka & Tonic",
+  "Vodka Tonic": "Vodka & Tonic",
   "Vodka & Lemon": "Vodka & Lemon",
+  "Vodka Lemon": "Vodka & Lemon",
   Paloma: "Paloma",
+  Mojito: "Mojito",
+  "Moscow Mule": "Moscow Mule",
+  Bellini: "Bellini",
 
   // Vino e Spumanti
   "Cantine E. Sammarco": "E. Sammarco Winery",
+  "E. Sammarco Winery": "E. Sammarco Winery",
   Prosecco: "Prosecco",
   "Costa D'Amalfi Terre Saracene": "Amalfi Coast Terre Saracene",
+  "Amalfi Coast Terre Saracene": "Amalfi Coast Terre Saracene",
+  "Vino Bianco": "White Wine",
+  "Vino Rosso": "Red Wine",
+  "Vino Rosato": "Rosé Wine",
+  Spumante: "Sparkling Wine",
 
   // Sottocategorie Food
   Pinse: "Pinse",
+  Piadine: "Piadine",
+  Panini: "Panini",
+  Bruschettone: "Bruschettone",
   Toast: "Toast",
   Fritti: "Fried Food",
+
+  // Altro
+  "Gran Tagliere": "Large Platter",
+  "Tagliere Misto": "Mixed Platter",
+  "Olive Ascolane": "Stuffed Olives",
+  Supplì: "Rice Balls",
 };
 
 export const menuDescriptionTranslations = {
+  // Descrizioni Bibite
+  "Tonica; Limone; Pompelmo Rosa": "Tonic; Lemon; Pink Grapefruit",
+  "Pesca; Pera; Ace": "Peach; Pear; Ace",
+
   // Descrizioni Pinse
   "Stracciata dei Monti Lattari, Pomodori Semidry Rossi, Pesto al Basilico":
     "Monti Lattari Stracciatella, Semi-dry Red Tomatoes, Basil Pesto",
 
   "Stracciata dei Monti Lattari, Filetti di Tonno, Zest di Limone Amalfitano":
     "Monti Lattari Stracciatella, Tuna Fillets, Amalfi Lemon Zest",
+
+  "Stracciata dei Monti Lattari, Filetti di Alici, Zest di Limone Amalfitano":
+    "Monti Lattari Stracciatella, Anchovy Fillets, Amalfi Lemon Zest",
 
   "Stracciata dei Monti Lattari, Pomodori Semidry Rossi, Melenzane Grigliate":
     "Monti Lattari Stracciatella, Semi-dry Red Tomatoes, Grilled Eggplant",
@@ -100,29 +169,82 @@ export const menuDescriptionTranslations = {
   "Zucchine Grigliate, Melenzane Grigliate, Pomodori Semidry Rossi":
     "Grilled Zucchini, Grilled Eggplant, Semi-dry Red Tomatoes",
 
-  // Descrizioni Toast
+  "Monti Lattari Stracciatella, Semi-dry Red Tomatoes, Basil Pesto":
+    "Monti Lattari Stracciatella, Semi-dry Red Tomatoes, Basil Pesto",
+
+  "Monti Lattari Stracciatella, Tuna Fillets, Amalfi Lemon Zest":
+    "Monti Lattari Stracciatella, Tuna Fillets, Amalfi Lemon Zest",
+
+  "Monti Lattari Stracciatella, Semi-dry Red Tomatoes, Grilled Eggplant":
+    "Monti Lattari Stracciatella, Semi-dry Red Tomatoes, Grilled Eggplant",
+
+  "Monti Lattari Stracciatella, Mortadella, Pistachio Crumble":
+    "Monti Lattari Stracciatella, Mortadella, Pistachio Crumble",
+
+  "Monti Lattari Stracciatella, Grilled Eggplant, Prosciutto":
+    "Monti Lattari Stracciatella, Grilled Eggplant, Prosciutto",
+
+  "Monti Lattari Stracciatella, Grilled Zucchini, Cooked Ham":
+    "Monti Lattari Stracciatella, Grilled Zucchini, Cooked Ham",
+
+  "Grilled Zucchini, Grilled Eggplant, Semi-dry Red Tomatoes":
+    "Grilled Zucchini, Grilled Eggplant, Semi-dry Red Tomatoes",
+
+  // Descrizioni Toast/Piadine/Panini
   "Cotto e sottiletta": "Ham and cheese",
+  "Ham and cheese": "Ham and cheese",
   "Crudo e Sottiletta": "Prosciutto and cheese",
+  "Prosciutto and cheese": "Prosciutto and cheese",
   "Salame e Sottiletta": "Salami and cheese",
+  "Salami and cheese": "Salami and cheese",
   "Mortadella e Sottiletta": "Mortadella and cheese",
+  "Mortadella and cheese": "Mortadella and cheese",
+
+  // Descrizioni Bruschettone
+  "Stracciata dei Monti Lattari e Prosciutto Cotto":
+    "Monti Lattari Stracciatella and Cooked Ham",
+  "Stracciata dei Monti Lattari e Prosciutto Crudo":
+    "Monti Lattari Stracciatella and Prosciutto",
+  "Stracciata dei Monti Lattari e Salame":
+    "Monti Lattari Stracciatella and Salami",
+  "Stracciata dei Monti Lattari e Mortadella":
+    "Monti Lattari Stracciatella and Mortadella",
 
   // Descrizioni Cocktail
   "Aperol, Prosecco, Soda": "Aperol, Prosecco, Soda",
   "Campari, Prosecco, Soda": "Campari, Prosecco, Soda",
   "Liquore al Sambuco, Prosecco, Soda, Menta":
     "Elderflower Liqueur, Prosecco, Soda, Mint",
+  "Elderflower Liqueur, Prosecco, Soda, Mint":
+    "Elderflower Liqueur, Prosecco, Soda, Mint",
   "Limoncello, Prosecco, Soda": "Limoncello, Prosecco, Soda",
   "Gin, Vermouth Rosso, Campari": "Gin, Red Vermouth, Campari",
+  "Gin, Red Vermouth, Campari": "Gin, Red Vermouth, Campari",
   "Prosecco, Vermouth Rosso, Campari": "Prosecco, Red Vermouth, Campari",
+  "Prosecco, Red Vermouth, Campari": "Prosecco, Red Vermouth, Campari",
   "Campari, Vermouth Rosso, Soda": "Campari, Red Vermouth, Soda",
+  "Campari, Red Vermouth, Soda": "Campari, Red Vermouth, Soda",
   "Gin, Tonica Orsini": "Gin, Orsini Tonic",
+  "Gin, Orsini Tonic": "Gin, Orsini Tonic",
   "Gin Hendrick's, Tonica Orsini": "Hendrick's Gin, Orsini Tonic",
+  "Hendrick's Gin, Orsini Tonic": "Hendrick's Gin, Orsini Tonic",
   "Gin Mare Capri, Tonica Orsini": "Gin Mare Capri, Orsini Tonic",
+  "Gin Mare Capri, Orsini Tonic": "Gin Mare Capri, Orsini Tonic",
   "Gin, Soda Limone Orsini": "Gin, Orsini Lemon Soda",
+  "Gin, Orsini Lemon Soda": "Gin, Orsini Lemon Soda",
   "Vodka, Tonica Orsini": "Vodka, Orsini Tonic",
+  "Vodka, Orsini Tonic": "Vodka, Orsini Tonic",
   "Vodka, Soda Limone Orsini": "Vodka, Orsini Lemon Soda",
+  "Vodka, Orsini Lemon Soda": "Vodka, Orsini Lemon Soda",
   "Tequila, Soda Pompelmo Orsini, Sciroppo di Agave, Succo di Limone":
     "Tequila, Orsini Grapefruit Soda, Agave Syrup, Lemon Juice",
+  "Tequila, Orsini Grapefruit Soda, Agave Syrup, Lemon Juice":
+    "Tequila, Orsini Grapefruit Soda, Agave Syrup, Lemon Juice",
+
+  // Descrizioni aggiuntive
+  "Supplemento di 1€ per ogni aggiunta": "€1 supplement for each addition",
+  "* Si prega di comunicare eventuali intolleranze o allergie":
+    "Please inform us of any intolerances or allergies",
 };
 
 export const menuFieldTranslations = {
@@ -134,6 +256,12 @@ export const menuFieldTranslations = {
     Aroma: "Aroma",
     "Metodo di produzione": "Metodo di produzione",
     Calice: "Calice",
+    Origin: "Provenienza",
+    Type: "Tipologia",
+    "Alcohol Content": "Gradazione",
+    Color: "Colore",
+    "Production Method": "Metodo di produzione",
+    Glass: "Calice",
   },
   en: {
     Provenienza: "Origin",
@@ -143,22 +271,123 @@ export const menuFieldTranslations = {
     Aroma: "Aroma",
     "Metodo di produzione": "Production Method",
     Calice: "Glass",
+    Origin: "Origin",
+    Type: "Type",
+    "Alcohol Content": "Alcohol Content",
+    Color: "Color",
+    "Production Method": "Production Method",
+    Glass: "Glass",
   },
 };
 
 export const menuValueTranslations = {
   // Provenienze
   Italia: "Italy",
+  Italy: "Italy",
   Campania: "Campania",
   Veneto: "Veneto",
+  Toscana: "Tuscany",
+  Piemonte: "Piedmont",
+  Sicilia: "Sicily",
+  Puglia: "Puglia",
+  "Repubblica Ceca": "Czech Republic",
+  Irlanda: "Ireland",
+  Germania: "Germany",
+  Belgio: "Belgium",
 
   // Tipologie
   "Bianco DOC": "White DOC",
+  "White DOC": "White DOC",
   "Rosato DOC": "Rosé DOC",
+  "Rosé DOC": "Rosé DOC",
   "Bassa fermentazione": "Bottom Fermentation",
+  "Bottom Fermentation": "Bottom Fermentation",
+  Lager: "Lager",
+  Stout: "Stout",
+  "Strong ale": "Strong Ale",
+  IPA: "IPA",
+  Weiss: "Wheat Beer",
+
+  // Colori
+  "Oro Brillante": "Bright Gold",
+  "Nero Corvo": "Raven Black",
+  "Rosso bruno": "Brown Red",
+  Ambrato: "Amber",
+  Dorato: "Golden",
+
+  // Schiuma
+  Compatta: "Compact",
+  Cremosa: "Creamy",
+  Persistente: "Persistent",
+
+  // Aroma
+  "Armonico luppolato": "Harmonious Hoppy",
+  Fruttato: "Fruity",
+  Floreale: "Floral",
+  Speziato: "Spicy",
 
   // Altre traduzioni di valori
   Calice: "Glass",
+  Glass: "Glass",
+  Media: "Medium",
+  Grande: "Large",
+  Piccola: "Small",
+};
+
+export const additionalTranslations = {
+  it: {
+    "Supplemento di 1€ per ogni aggiunta":
+      "Supplemento di 1€ per ogni aggiunta",
+    "Si prega di comunicare eventuali intolleranze o allergie":
+      "Si prega di comunicare eventuali intolleranze o allergie",
+    // Sottocategorie
+    Pinse: "Pinse",
+    Piadine: "Piadine",
+    Panini: "Panini",
+    Bruschettone: "Bruschettone",
+    Toast: "Toast",
+    Fritti: "Fritti",
+    "Soft Drinks": "Bibite",
+    Beers: "Birre",
+    Food: "Food",
+    "Coffee Bar": "Caffetteria",
+    Cocktails: "Cocktail",
+    "Wine & Sparkling": "Vino e Spumanti",
+    // Categorie principali
+    bibite: "Bibite",
+    birre: "Birre",
+    food: "Food",
+    caffetteria: "Caffetteria",
+    cocktail: "Cocktail",
+    altro: "Altro",
+    "vino-e-spumanti": "Vino e Spumanti",
+  },
+  en: {
+    "Supplemento di 1€ per ogni aggiunta": "€1 supplement for each addition",
+    "Si prega di comunicare eventuali intolleranze o allergie":
+      "Please inform us of any intolerances or allergies",
+    // Sottocategorie
+    Pinse: "Pinse",
+    Piadine: "Piadine",
+    Panini: "Panini",
+    Bruschettone: "Bruschettone",
+    Toast: "Toast",
+    Fritti: "Fried Food",
+    "Soft Drinks": "Soft Drinks",
+    Beers: "Beers",
+    Food: "Food",
+    "Coffee Bar": "Coffee Bar",
+    Cocktails: "Cocktails",
+    "Wine & Sparkling": "Wine & Sparkling",
+    // Categorie principali
+    bibite: "Soft Drinks",
+    birre: "Beers",
+    food: "Food",
+    caffetteria: "Coffee Bar",
+    cocktail: "Cocktails",
+    altro: "Other",
+    "vino-e-spumanti": "Wine & Sparkling",
+  },
 };
 
 // Funzione helper per tradurre gli elementi del menu
@@ -190,4 +419,9 @@ export function translateMenuItem(item, language) {
 // Funzione per tradurre le etichette dei campi
 export function translateFieldLabel(fieldName, language) {
   return menuFieldTranslations[language][fieldName] || fieldName;
+}
+
+// Funzione per tradurre testi aggiuntivi
+export function translateAdditional(text, language) {
+  return additionalTranslations[language][text] || text;
 }
